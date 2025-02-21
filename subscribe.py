6c16237f003efcc5051a5c799f4c2a81811c827f
thaibahung <tbhung1001@gmail.com>
2025-02-21 15:46:00 +0700

    Update Subscribe

--- subscribe.py
+++ subscribe.py
@@ -9,16 +9,33 @@ async def subscribe():
         async with websockets.connect(ETH_WS_URL) as websocket:
             print("Successful connected to Ethereum Websocket")
 
+            '''
+            - jsonrpc -> Defines the JSON-RPC protocol version -> (= 2.0) Ensures compatibility with Ethereum JSON-RPC nodes
+            - id -> A unique identifier for the request -> Used to match the response with the request
+            - method -> Specifies the Ether JSON-RPC function -> "eth_subscribe" tells Ether to create a Websocket subscription
+            - params -> holds the arguments for "method" 
+            '''
+
             # Send a sub request to listen for new blocks
+            '''
             subscription_request = {
                 "jsonrpc": "2.0",
                 "id": 1,
                 "method": "eth_subscribe",
                 "params": ["newHeads"]
             }
+            '''
+
+            # Send a sub request for pending tx in the mempool.=
+            subscription_request = {
+                "jsonrpc": "2.0",
+                "id": 2,
+                "method": "eth_subscribe",
+                "params": ["newPendingTransactions"]
+            }
 
             await websocket.send(json.dumps(subscription_request))
-            print("Subscribed to new block headers!")
+            print("Subscribed to new information!")
 
             while True:
                 response = await websocket.recv()
